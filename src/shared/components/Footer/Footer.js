fa15e92f6c68060ee7b9f609f3ec3c1e71c5b189
Jiri <kalendar.skolavrchlabi@gmail.com>
2022-06-20 23:05:07 +0200

    colors

--- src/shared/components/Footer/Footer.js
+++ src/shared/components/Footer/Footer.js
@@ -1,8 +1,10 @@
 import React from "react";
+import './Footer.css';
 
 const Footer = () => {
   return (
     <footer>
+      <div className="footer-strip"></div>
       <p className="footer-text">2022 © Univerzita Hradec Králové</p>
     </footer>
   );
