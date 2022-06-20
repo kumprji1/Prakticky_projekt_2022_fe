d59aad736ea83ee0b3c0c6e3abd6bcb64f4aa7ac
Jiri <kalendar.skolavrchlabi@gmail.com>
2022-06-20 22:19:06 +0200

    artisec_changes

--- src/shared/components/Header/Header.js
+++ src/shared/components/Header/Header.js
@@ -1,37 +1,43 @@
 import React, { Fragment, useState } from "react";
 
-import UhkLogo from "../../../img/fim-uhk-cs_hor.svg";
+import ArtisecLogo from "../../../img/artisec_pod_zamkem.jpg";
 import Backdrop from "../UI/Backdrop/Backdrop";
 
-import './Header.css'
+import "./Header.css";
 
 const Header = () => {
   const [showMobileNav, setShowMobileNav] = useState(false);
 
   return (
     <Fragment>
-        <nav className={`mobile_main_nav ${!showMobileNav && 'hide-mobile-nav'}`}>
-          <ul className="mobile_main_nav__links">
-            <li className="mobile_main_nav__link">O projektu</li>
-            <li className="mobile_main_nav__link">Workshopy</li>
-            {/* <li className="mobile_main_nav__link">Výstupy</li> */}
-          </ul>
-        </nav>
-      {showMobileNav && <Backdrop clear={() => setShowMobileNav(false)}/>}
+      <nav className={`mobile_main_nav ${!showMobileNav && "hide-mobile-nav"}`}>
+        <ul className="mobile_main_nav__links">
+          <li className="mobile_main_nav__link">O projektu</li>
+          <li className="mobile_main_nav__link">Workshopy</li>
+          <li className="mobile_main_nav__link">Výstupy</li>
+          <li className="mobile_main_nav__link">Kontakty</li>
+          {/* <li className="mobile_main_nav__link">Výstupy</li> */}
+        </ul>
+      </nav>
+      {showMobileNav && <Backdrop clear={() => setShowMobileNav(false)} />}
       <header>
-        <img className="fim_uhk_logo" src={UhkLogo} alt="FIM UHK" />
-        <div className="hamburger" onClick={() => setShowMobileNav(true)}>
-          <div className="hamburger_strip"></div>
-          <div className="hamburger_strip"></div>
-          <div className="hamburger_strip"></div>
-        </div>
-        <nav className="main_nav">
-          <ul className="main_nav__links">
-            <li className="main_nav__link">O projektu</li>
-            <li className="main_nav__link">Workshopy</li>
-            {/* <li className="main_nav__link">Výstupy</li> */}
-          </ul>
-        </nav>
+        {/* <div className="header_content--wrapper"> */}
+          <img className="artisec_logo" src={ArtisecLogo} alt="Artisec Logo" />
+          <div className="hamburger" onClick={() => setShowMobileNav(true)}>
+            <div className="hamburger_strip"></div>
+            <div className="hamburger_strip"></div>
+            <div className="hamburger_strip"></div>
+          </div>
+          <nav className="main_nav">
+            <ul className="main_nav__links">
+              <li className="main_nav__link active_link">O projektu</li>
+              <li className="main_nav__link">Workshopy</li>
+              <li className="main_nav__link">Výstupy</li>
+              <li className="main_nav__link">Kontakty</li>
+              {/* <li className="main_nav__link">Výstupy</li> */}
+            </ul>
+          </nav>
+        {/* </div> */}
       </header>
     </Fragment>
   );
--- src/shared/pages/FirstPage.js
+++ src/shared/pages/FirstPage.js
@@ -1,32 +1,47 @@
 import React, { Fragment } from "react";
+import ArtisecLogo from "../../img/artisec_pod_zamkem.jpg";
+import ClaimLogo from "../../img/CLAIM_LOGO_RGB.png";
+import InovaceLogo from "../../img/plus-inovace_Logo-barva-negativ_RGB.jpg";
 
 const FirstPage = () => {
   return (
     <Fragment>
       <section className="project_section">
-        <h1 className="project_name_label">Projekt</h1>
-        <h1 className="project_name">
-          Využití umělé inteligence pro zajištění kybernetické bezpečnosti Smart
-          City
-        </h1>
-        <p className="project_description">
-          Hlavním záměrem je navrhnout a ověřit komplexní bezpečnostní model
-          využití umělé inteligence pro zajištění kybernetické bezpečnosti Smart
-          City založený na datové analýze primárních a podpůrných datových aktiv
-          využívaných v rozsahu Smart City. Model využití umělé inteligence pro
-          zajištění kybernetické bezpečnosti založený na datových aktivech
-          umožní vytvoření a následné ověření víceúrovňového bezpečnostního
-          modelu řízeného dle procesů definovaných v mezinárodně uznávaném
-          systému pro řízení a správy IT služeb ITIL splňujícího bezpečnostní
-          kritéria dle ČSN EN ISO / IEC 27001. Pro zajištění Demingova cyklu
-          PDCA s ohledem na udržitelnost celého bezpečnostního systému budou v
-          rámci řešení nasazeny prostředky umělé inteligence.
-        </p>
-        <a className="uhk_project_link" href="https://www.uhk.cz/cs/fakulta-informatiky-a-managementu/veda-a-vyzkum/vav-projekty/projekty-ministerstva-vnitra-cr">
-          Přejít na univerzitní stránky
+        <div className="project_and_logo--wrapper">
+          <div className="project_section--left">
+            <h1 className="project_name_label">Projekt</h1>
+            <h1 className="project_name">
+              Využití umělé inteligence pro zajištění kybernetické bezpečnosti
+              Smart City
+            </h1>
+            <div className="project_names">
+              <span>1. řešitel</span>
+              <span>2. řešitel</span>
+              <span>aplikační garant</span>
+            </div>
+          </div>
+          <div className="project_section--right">
+            <img
+              className="project_section__artisec_logo"
+              src={ArtisecLogo}
+              alt="Artisec Logo"
+            />
+          </div>
+        </div>
+        <a
+          className="uhk_project_link"
+          href="https://www.uhk.cz/cs/fakulta-informatiky-a-managementu/veda-a-vyzkum/vav-projekty/projekty-ministerstva-vnitra-cr"
+        >
+          Více o projektu
         </a>
       </section>
-      <section className="workshop_section">
+      <section className="sponzori_section">
+        <div className="sponzori_section__content_wrapper">
+          <img className="sponzors_logo" src={ClaimLogo} alt="Claim Logo" />
+          <img className="sponzors_logo" src={InovaceLogo} alt="Inovace Logo" />
+        </div>
+      </section>
+      {/* <section className="workshop_section">
         <div className="workshop_section__content_wrapper">
           <div className="first_workshop__item">
             <div className="first_workshop__item--title_wrapper">
@@ -50,7 +65,7 @@ const FirstPage = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
     </Fragment>
   );
 };
